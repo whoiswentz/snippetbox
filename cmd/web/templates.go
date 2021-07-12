ccfb10d25fe46d0792ca95731acc7efd3852fc9f
Vinicios Wentz <vinicios@wentz.io>
2021-07-11 21:23:18 -0300

    feature: template functions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,7 +3,16 @@ package main
 import (
 	"path/filepath"
 	"text/template"
+	"time"
 )
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
 
 func NewTemplateCache(dir string) (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
@@ -16,7 +25,7 @@ func NewTemplateCache(dir string) (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
