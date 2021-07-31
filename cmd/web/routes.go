ead197a5333511747ca1a380736df7c9d23a4573
Vinicios Wentz <vinicios@wentz.io>
2021-07-31 19:55:44 -0300

    refactor: change middleware order

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,27 +8,30 @@ import (
 func (app *application) routes() http.Handler {
 	m := mux.NewRouter()
 
-	m.Handle("/", app.session.Enable(http.HandlerFunc(app.home))).Methods(http.MethodGet)
+	m.Handle("/", app.session.Enable(noSurf(app.Authenticated(http.HandlerFunc(app.home))))).Methods(http.MethodGet)
 
-	m.Handle("/snippet/create", app.session.Enable(app.Authenticated(app.RequireAuthentication(http.HandlerFunc(app.createSnippetForm))))).
+	m.Handle("/snippet/create", app.session.Enable(noSurf(app.Authenticated(app.RequireAuthentication(http.HandlerFunc(app.createSnippetForm)))))).
 		Methods(http.MethodGet)
-	m.Handle("/snippet/create", app.session.Enable(app.Authenticated(app.RequireAuthentication(http.HandlerFunc(app.createSnippet))))).
+	m.Handle("/snippet/create", app.session.Enable(noSurf(app.Authenticated(app.RequireAuthentication(http.HandlerFunc(app.createSnippet)))))).
 		Methods(http.MethodPost)
-	m.Handle("/snippet/{id}", app.session.Enable(app.Authenticated(http.HandlerFunc(app.showSnippet)))).
+	m.Handle("/snippet/{id}", app.session.Enable(noSurf(app.Authenticated(http.HandlerFunc(app.showSnippet))))).
 		Methods(http.MethodGet)
 
-	m.Handle("/user/signup", app.session.Enable(app.Authenticated(http.HandlerFunc(app.signupUserForm)))).
+	m.Handle("/user/signup", app.session.Enable(noSurf(app.Authenticated(http.HandlerFunc(app.signupUserForm))))).
 		Methods(http.MethodGet)
-	m.Handle("/user/signup", app.session.Enable(app.Authenticated(http.HandlerFunc(app.signupUser)))).Methods(http.MethodPost)
-	m.Handle("/user/login", app.session.Enable(app.Authenticated(http.HandlerFunc(app.loginUserForm)))).Methods(http.MethodGet)
-	m.Handle("/user/login", app.session.Enable(app.Authenticated(http.HandlerFunc(app.loginUser)))).Methods(http.MethodPost)
-	m.Handle("/user/logout", app.session.Enable(app.Authenticated(app.RequireAuthentication(http.HandlerFunc(app.logoutUser))))).
+	m.Handle("/user/signup", app.session.Enable(noSurf(app.Authenticated(http.HandlerFunc(app.signupUser))))).
+		Methods(http.MethodPost)
+	m.Handle("/user/login", app.session.Enable(noSurf(app.Authenticated(http.HandlerFunc(app.loginUserForm))))).
+		Methods(http.MethodGet)
+	m.Handle("/user/login", app.session.Enable(noSurf(app.Authenticated(http.HandlerFunc(app.loginUser))))).
+		Methods(http.MethodPost)
+	m.Handle("/user/logout", app.session.Enable(noSurf(app.Authenticated(app.RequireAuthentication(http.HandlerFunc(app.logoutUser)))))).
 		Methods(http.MethodPost)
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	m.PathPrefix("/static/").Handler(http.StripPrefix("/static", fileServer))
 
-	middlewaredMux := app.RecoverPanic(app.LogRequest(noSurf(SecureHeaders(m))))
+	middlewaredMux := app.RecoverPanic(app.LogRequest(SecureHeaders(m)))
 
 	return middlewaredMux
 }
